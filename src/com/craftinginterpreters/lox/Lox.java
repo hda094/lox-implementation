dc8dab63c01c15b4dcac0b0d54ee323c0aa9f7e1
Henrik Dalstø <henrik.dalsto@gmail.com>
2022-07-09 22:12:32 +0200

    added lexeme enum and TokenType

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 
 public class Lox {
+    static boolean hadError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -25,6 +26,9 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        //Indicate an error in the exit code.
+        if (hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -38,15 +42,28 @@ public class Lox {
                 break;
             }
             run(line);
+            hadError = false;
         }
     }
 
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
-        
+
         for (Token token : tokens) {
             System.out.println(token);
         }
     }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
+
 }
